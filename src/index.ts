48a427c5fdba6a1ca78d839a0d035947c82d215f
Kamesuta <kamesuta@gmail.com>
2024-01-09 15:26:32 +0900

    起動メッセージ

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Client, Events, GatewayIntentBits, Partials } from 'discord.js';
 import dotenv from 'dotenv';
 import { onVoiceCreateInteraction } from './guildProcess/voiceCreateInteraction.js';
 import { onVoiceStateUpdate } from './guildProcess/voiceCreate.js';
+import { logger } from './utils/log.js';
 
 // .envファイルを読み込む
 dotenv.config();
@@ -24,6 +25,9 @@ const client: Client = new Client({
 // -----------------------------------------------------------------------------------------------------------
 client.on(Events.InteractionCreate, onVoiceCreateInteraction);
 client.on(Events.VoiceStateUpdate, onVoiceStateUpdate);
+client.on(Events.ClientReady, () => {
+  logger.info(`${client.user?.username ?? 'Unknown'} として起動しました!`);
+});
 
 // Discordにログインする
 await client.login(process.env.DISCORD_TOKEN);
