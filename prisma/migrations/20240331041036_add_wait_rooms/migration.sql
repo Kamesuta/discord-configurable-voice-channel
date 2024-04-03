99af6c7f304898d02ed35be50a71795c56544a8a
Kamesuta <kamesuta@gmail.com>
2024-04-04 00:45:32 +0900

    migration修正

--- prisma/migrations/20240331041036_add_wait_rooms/migration.sql
+++ prisma/migrations/20240331041036_add_wait_rooms/migration.sql
@@ -4,5 +4,6 @@ CREATE TABLE `RoomLists` (
     `channel_id` VARCHAR(191) NOT NULL UNIQUE,
     `wait_channel_id` VARCHAR(191),
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `RoomLists_channel_id_key` (`channel_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
