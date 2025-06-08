946e6cf32537718c5f176984f32c7b564864b259
Kamesuta <kamesuta@gmail.com>
2025-06-08 18:14:39 +0900

    fix: lint修正

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 // 必要なパッケージをインポートする
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv';
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
 
-import { logger } from './utils/log.js';
-import { updateControlPanel } from './voiceController.js';
-import { onVoiceCreateInteraction } from './voiceInteractionHandler.js';
-import { onVoiceStateUpdate } from './voiceStateHandler.js';
-import { registerVoiceStatusHandler } from './voiceStatusHandler.js';
+import { logger } from "./utils/log.js";
+import { updateControlPanel } from "./voiceController.js";
+import { onVoiceCreateInteraction } from "./voiceInteractionHandler.js";
+import { onVoiceStateUpdate } from "./voiceStateHandler.js";
+import { registerVoiceStatusHandler } from "./voiceStatusHandler.js";
+import { nowait } from "./utils/utils.js";
 
 // .envファイルを読み込む
 dotenv.config();
@@ -27,14 +28,17 @@ export const client: Client = new Client({
 // -----------------------------------------------------------------------------------------------------------
 // イベントハンドラーを登録する
 // -----------------------------------------------------------------------------------------------------------
-client.on(Events.InteractionCreate, onVoiceCreateInteraction);
-client.on(Events.VoiceStateUpdate, onVoiceStateUpdate);
-client.on(Events.ClientReady, async () => {
-  logger.info(`${client.user?.username ?? 'Unknown'} として起動しました!`);
+client.on(Events.InteractionCreate, nowait(onVoiceCreateInteraction));
+client.on(Events.VoiceStateUpdate, nowait(onVoiceStateUpdate));
+client.on(
+  Events.ClientReady,
+  nowait(async () => {
+    logger.info(`${client.user?.username ?? "Unknown"} として起動しました!`);
 
-  // VC操作パネルのメッセージを投稿する
-  await updateControlPanel();
-});
+    // VC操作パネルのメッセージを投稿する
+    await updateControlPanel();
+  }),
+);
 registerVoiceStatusHandler();
 
 // Discordにログインする
